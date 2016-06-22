760bd97fead786854f8022f255c86eefe77ab9ba
Alessio Manai <alessio@manai.tk>
2016-06-22 14:48:18 +0200

    Rewrite class

--- src/isw/bandicootbrothers/condivideo/Main.java
+++ src/isw/bandicootbrothers/condivideo/Main.java
@@ -1,9 +1,9 @@
 package isw.bandicootbrothers.condivideo;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-	// write your code here
+public class Main{
+
+    public static void main(String[] args){
+
+        System.out.println("Condivideo");
     }
 }
