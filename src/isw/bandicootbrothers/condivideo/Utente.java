a7e702ccff82e0643194755168ae5fbcd80f04a9
Alessio Manai <alessio@manai.tk>
2016-06-22 10:50:06 +0200

    Prova commit branch unstable della classe Utente

---
+++ src/isw/bandicootbrothers/condivideo/Utente.java
@@ -0,0 +1,17 @@
+package isw.bandicootbrothers.condivideo;
+
+import java.util.ArrayList;
+
+/**
+ * Created by alessio on 22/06/16.
+ */
+public class Utente {
+
+    public Utente(){};
+
+    //Video visualizzaVideo();
+
+    void condividiVideo(){};
+
+    //ArrayList<Video> cercaVideo(){};
+}
