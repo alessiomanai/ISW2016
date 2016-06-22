913ec3921dc92f5354e7e2a8fa3f94853715f824
emanuelfois <emanuel.fois@gmail.com>
2016-06-22 11:34:19 +0200

    Creazione UtenteRegistrato

--- src/isw/bandicootbrothers/condivideo/UtenteRegistrato.java
+++ src/isw/bandicootbrothers/condivideo/UtenteRegistrato.java
@@ -4,4 +4,26 @@ package isw.bandicootbrothers.condivideo;
  * Created by emanuelfois on 22/06/16.
  */
 public class UtenteRegistrato extends Utente  {
+
+   private String username;
+        private String nome;
+        private String email;
+        private String password;
+
+    boolean login(){
+
+        System.out.println("Inserisci username");
+        System.out.println("Inserisci password");
+        System.out.println("Login effettuato");
+    }
+    void uploadVideo(){
+
+
+
+
+}
+    void seguiUtente(){
+
+
+    }
 }
