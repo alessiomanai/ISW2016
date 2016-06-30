30730337b62d67d8721f52b01e79df65f7f44461
Alessio Manai <alessio@manai.tk>
2016-06-30 17:12:19 +0200

    caricamento videosingleton e commento

---
+++ src/isw/bandicootbrothers/condivideo/Commento.java
@@ -0,0 +1,11 @@
+public class Commento
+{
+    String testo;
+    Utente autore;
+
+    public Commento (String testo, Utente autore)
+    {
+        this.testo = testo;
+        this.autore = autore;
+    }
+}
---
+++ src/isw/bandicootbrothers/condivideo/VideoSingleton.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class VideoSingleton {
+    /*
+    | La clase VideoSongleton funge da DATABASE DEI VIDEO.
+    | Esso è rappresentato da una singleton accessibile da qualsiasi punto del programma.
+    | Tale singleton è un arrayList di Video.
+    |
+    | Il database è stato popolato a mano da un costrutto Factory.
+    */
+
+    //Rendo la classe una singleton
+    private static VideoSingleton singleton;
+
+    //Arraylist che conterrà i video.
+    private ArrayList<Video> videoList = new ArrayList<Video>();
+
+    //Istanzio il metodo getInstance della singleton
+    public static VideoSingleton getInstance() {
+        if (singleton == null) {
+            singleton = new VideoSingleton();
+        }
+        return singleton;
+    }
+
+    //La factory popola l'array-database
+    private VideoSingleton() {
+
+    }
+
+    public ArrayList<Video> getVideoList() {
+        return videoList;
+    }
+}
